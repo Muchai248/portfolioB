87ebafd51faebbd537527183f8f9cceb88d033ff
Muchai248 <muchaimark6@gmail.com>
2023-12-15 11:20:10 +0300

    new commit

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Container() {
   return (
     <>
-    <header className="fixed items-center text-5xl font-medium left-52 top-44 ">Mark Muchai Mwaura</header><br/>
-    <div className="fixed left-52 top-64">
-      <h1 className="flex items-center text-4xl font-medium ">
-        I build
+    <header className="fixed items-center text-5xl font-medium left-52 top-1/3 text-green-500 ">Mark Muchai Mwaura</header><br/>
+    <div className="fixed left-52 top-1/2">
+      <h1 className="flex items-center text-5xl font-medium ">I build</h1>
+      <h2 className="flex items-center text-4xl font-medium ">
         <span className="relative ml-3 h-[1em] w-80 overflow-hidden">
           <span className="absolute h-full w-full -translate-y-full animate-slide leading-none text-white [animation-delay:1.25s]">
             Components
@@ -21,9 +21,12 @@ function Container() {
             Animations
           </span>
         </span>
-      </h1>
+      </h2>
       <br />
+      <h3 className="flex items-center text-5xl font-medium">for the web.</h3>
+      <br/>
       <hr className="fixed border-2 border-orange-500 w-1/2"></hr>
+      <p className="fixed items-center text-6xl font-medium left-52 top-3/4">A software engineer based in Africa who builds applications and websites.</p>
     </div>
     </>
   );
